143ebc77bbdc05f722e9e43072270bdee65ee706
Taylor Curran <taylorcurran@Taylors-MacBook-Air.local>
2021-11-23 07:45:10 -0500

    leaning about intents but struggling with the Guild instantiation

--- bot2.py
+++ bot2.py
@@ -20,7 +20,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    print(client.Member)
 
 @client.event
 async def on_message(message):
