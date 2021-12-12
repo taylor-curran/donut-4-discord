8ace9d323af81ec1c48568f6a66294f3e01d6cd9
Taylor Curran <taylorcurran@Taylors-MacBook-Air.local>
2021-12-12 13:10:21 -0500

    Still messing with it

--- donut_4_discord/members.py
+++ donut_4_discord/members.py
@@ -1,3 +1,27 @@
 import pandas as pd
-import random
+import numpy as np
+
+def get_pairs(members):
+
+    df = pd.DataFrame({'members': members})
+    df_rand = df.sample(frac=1)
+    # reset index
+
+    odd = [i for i in df.index if i % 2 != 0]
+    even = [i for i in df.index if i % 2 == 0]
+
+    pairs = []
+    for i in range(len(odd)):
+
+        # DUH AttributeError: 'list' object has no attribute 'iloc'
+        pairs.append([.iloc[i], even.iloc[i]])
+
+    return pairs
+
+
+
+toy_df = df = pd.DataFrame({'members': ['tay', 'joe', 'mo', 'tier', 'alex']})
+
+
+
 
